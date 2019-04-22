20be620d0875b34da1f2e9962176e7859eb22132
CyberImpXIII <jnelsenepstein@gmail.com>
2019-04-22 15:38:19 -0500

    we did it I think. The list shrinks when ya click

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-// TODO
+    let list = ["Banana", "Swordfish", "Avocado", "Kimchee", "Corn"];
+    class GroceryList extends React.Component{
+      constructor(props){
+          super(props)
+          this.state = {
+            actualList: list, 
+            done: false
+          }
+          this.onListItemClick = this.onListItemClick.bind(this);
+    }
+
+      onListItemClick(){
+          let newList = this.state.actualList.slice();
+          newList.shift();
+          this.setState({
+            actualList: newList
+          });
+        }
+
+      render(){
+        return(<ul>
+            <li onClick={this.onListItemClick}>{this.state.actualList[0]}</li>
+            <li>{this.state.actualList[1]}</li>
+            <li>{this.state.actualList[2]}</li>
+        </ul>
+        )
+      }
+    }
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
